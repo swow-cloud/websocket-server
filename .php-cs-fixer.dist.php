68de1d7a5d076918edadb3de66a93cddf21bc021
he ping <847050412@qq.com>
2021-10-19 18:35:53 +0800

    init

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+$header = <<<'EOF'
+This file is part of SwowCloud
+@license  https://github.com/swow-cloud/music-server/blob/main/LICENSE
+EOF;
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        '@Symfony' => true,
+        '@DoctrineAnnotation' => true,
+        '@PhpCsFixer' => true,
+        'header_comment' => [
+            'comment_type' => 'PHPDoc',
+            'header' => $header,
+            'separate' => 'bottom',
+            'location' => 'after_open',
+        ],
+        'array_syntax' => [
+            'syntax' => 'short',
+        ],
+        'list_syntax' => [
+            'syntax' => 'short',
+        ],
+        'concat_space' => [
+            'spacing' => 'one',
+        ],
+        'blank_line_before_statement' => [
+            'statements' => [
+                'declare',
+                'return',
+            ],
+        ],
+        'blank_line_after_namespace' => true,
+        'braces' => [
+            'allow_single_line_closure' => true,
+        ],
+        'general_phpdoc_annotation_remove' => [
+            'annotations' => [
+                'author',
+            ],
+        ],
+        'ordered_imports' => [
+            'imports_order' => [
+                'class',
+                'function',
+                'const',
+            ],
+            'sort_algorithm' => 'alpha',
+        ],
+        'single_line_comment_style' => [
+            'comment_types' => [
+            ],
+        ],
+        'yoda_style' => [
+            'always_move_variable' => false,
+            'equal' => false,
+            'identical' => false,
+        ],
+        'phpdoc_align' => [
+            'align' => 'left',
+        ],
+        'multiline_whitespace_before_semicolons' => [
+            'strategy' => 'no_multi_line',
+        ],
+        'constant_case' => [
+            'case' => 'lower',
+        ],
+        'operator_linebreak' => [
+            'only_booleans' => true,
+            'position' => 'end',
+        ],
+        'class_attributes_separation' => true,
+        'combine_consecutive_unsets' => true,
+        'declare_strict_types' => true,
+        'linebreak_after_opening_tag' => true,
+        'lowercase_static_reference' => true,
+        'no_useless_else' => true,
+        'no_unused_imports' => true,
+        'no_unneeded_curly_braces' => false,
+        'not_operator_with_space' => false,
+        'not_operator_with_successor_space' => false,
+        'ordered_class_elements' => false,
+        'php_unit_strict' => false,
+        'phpdoc_separation' => false,
+        'phpdoc_summary' => false,
+        'single_quote' => true,
+        'increment_style' => false,
+        'standardize_increment' => false,
+        'standardize_not_equals' => true,
+        'multiline_comment_opening_closing' => true,
+        // NOTE: this is for anonymous functions using WaitReference, maybe there's better solution
+        'lambda_not_used_import' => false,
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+                         ->exclude('vendor')
+                         ->exclude('storage')
+                         ->exclude('runtimes')
+                         ->in(__DIR__)
+    )
+    ->setUsingCache(false);
---
+++ src/Application.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of SwowCloud
+ * @license  https://github.com/swow-cloud/music-server/blob/main/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace SwowCloud\MusicServer;
+
+use Symfony\Component\Console\Application as SymfonyApplication;
+
+class Application extends SymfonyApplication
+{
+}
