d7e98552324f07908bb71b1c036a436df71079d6
he ping <847050412@qq.com>
2021-11-18 10:50:12 +0800

    签名

---
+++ config/autoload/jws.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of SwowCloud
+ * @license  https://github.com/swow-cloud/music-server/blob/main/LICENSE
+ */
+
+declare(strict_types=1);
+
+use Jose\Component\Signature\Algorithm\HS256;
+use Jose\Component\Signature\Serializer\CompactSerializer;
+
+/*
+ * JWS
+ * @link https://web-token.spomky-labs.com/the-components/encrypted-tokens-jwe/jwe-loading
+ */
+return [
+    'key' => env('JSON_WEBTOKEN_KEY'),
+    'kty' => env('JSON_WEBTOKEN_KTY'),
+    'serializer' => CompactSerializer::class,
+    'signature_algorithms' => HS256::class,
+];
