46dc04e40c51807314a8d4363eafb84946d71419
he ping <847050412@qq.com>
2022-06-09 09:25:23 +0800

    t

---
+++ tests/php/pcntl.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+$redis = new Redis();
+$redis->connect('127.0.0.1',6379);
+$pid = pcntl_fork(); //fork一个子进程
+if ($pid > 0) { //父进程会获取到fork到的子进程的进程id
+    var_dump($redis);
+    var_dump('我是父进程');
+} elseif ($pid == 0) { //$pid=0时为刚刚派生出的子进程
+    var_dump('我是子进程');
+    var_dump($redis);
+} else {
+    var_dump('创建子进程失败');
+}
